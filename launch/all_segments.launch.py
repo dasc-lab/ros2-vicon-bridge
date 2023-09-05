b87b13c1e61b475af533740b2fea52baeb09164d
devansh <devansh@umich.edu>
2023-09-05 12:11:35 -0400

    new vicon ip

--- launch/all_segments.launch.py
+++ launch/all_segments.launch.py
@@ -3,7 +3,7 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
 
-    vicon_computer_ip = "192.168.2.136" # ip address of vicon computer
+    vicon_computer_ip = "192.168.1.164" # ip address of vicon computer
     port_number = "801" # default port number
     hostname = f"{vicon_computer_ip}:{port_number}"
 
