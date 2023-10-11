7e68fd96ea68ce06cc0efaaee6da0edae818cc43
Devansh <devanshinspace@gmail.com>
2023-10-11 12:27:43 -0400

    update launch file

--- launch/all_segments.launch.py
+++ launch/all_segments.launch.py
@@ -3,22 +3,22 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
 
-    vicon_computer_ip = "192.168.1.164" # ip address of vicon computer
+    vicon_computer_ip = "192.168.1.115" # ip address of vicon computer
     port_number = "801" # default port number
-    hostname = f"{vicon_computer_ip}:{port_number}"
+    host_name = f"{vicon_computer_ip}:{port_number}"
 
     return LaunchDescription([
         Node(
             package='vicon_bridge',
             executable='vicon_bridge',
             name='vicon_bridge',
-            # parameters = [
-            #     {"hostname": hostname},
-            #     {"stream_mode": "ServerPush"}, # or "ClientPull"
-            #     {"update_rate_hz": 250.0},
-            #     {"publish_specific_segment": False},
-            #     {"world_frame_id": "world"},
-            #     {"tf_namespace": "vicon"}
-            #     ]
+            parameters = [
+                {"host_name": host_name},
+            #    {"stream_mode": "ServerPush"}, # or "ClientPull"
+            #    {"update_rate_hz": 250.0},
+            #    {"publish_specific_segment": False},
+            #    {"world_frame_id": "world"},
+            #    {"tf_namespace": "vicon"}
+            ]
         ),
     ])
